e55b92186cf6164403f6ec7644bdf49d291b065f
Linyi <linyis2222@gmail.com>
2025-12-18 21:12:42 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const demons = [
+  {
+    name: "Thinking Space II",
+    creators: "Jawis, Rin04, HaydenDom, V453C70M14, UFokinWotM8, Hyperbola, NcaT, KugelBlitZ, Triforce, Maxmur, MeatiusGaming, DrCuber, ADrone, Korewa, GhostVandalf, CairoX, RealVet, Genoxide, Crowlley, Meox, GamerKnight750, ThunderDarkness, DiamondSkull, Kevellium",
+    verifier: "zZoink",
+    publisher: "CairoX",
+    video: "https://www.youtube.com/embed/CELNmHwln_c",
+    id: "119544028",
+    password: "Free to Copy"
+    tag: "abc",
+  },
+  {
+    name: "Amethyst",
+    creators: "iMist, IvashkaUA, NoobyPro27, MinAY, BeClaNin0, Titoalper, Roselyne, ItsxEcho, GamerKnight750, Uriun112, 3DADronX, SnomGMD, Bedrock",
+    verifier: "wPopoff",
+    publisher: "iMist",
+    video: "https://www.youtube.com/embed/4lfkzz1VCbA",
+    id: "119550490",
+    password: "Free to Copy"
+  },
+  {
+    name: "Flamewall",
+    creators: "UNarwall & more(125 people)",
+    verifier: "Cuatrocientos",
+    publisher: "UNarwall",
+    video: "https://www.youtube.com/embed/x4Io4zkWVRw",
+    id: "126242564",
+    password: "Free to Copy"
+  }
+  ];
+
+/* ===========================
+   changeLog 데이터 (요청한 예시 2개 포함)
+   - 'detail' 필드에 단순 텍스트(글)만 들어갑니다.
+   - 날짜는 문자열(YYYY-MM-DD 또는 로컬 포맷)로 표기.
+   =========================== */
+const changeLog = [
+  { date: '2025-12-18', detail: 'niwa has been placed at #1.' },
+  { date: '2025-12-18', detail: 'List Created' },
+  
